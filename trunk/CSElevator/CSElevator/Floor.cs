7a849c5ee0107cfc3b4046decb966c009c5795a5
jsgydjq@gmail.com <jsgydjq@gmail.com@abe33d53-0549-ff03-17a0-e676305ee19f>
2012-11-20 15:18:54 +0000

    Detailed Class Floor

--- trunk/CSElevator/CSElevator/Floor.cs
+++ trunk/CSElevator/CSElevator/Floor.cs
@@ -7,5 +7,16 @@ namespace CSElevator
 {
     class Floor
     {
+        public int position;
+        public Queue<Person> upQueue;
+        public Queue<Person> downQueue;
+        public Floor(int pos)
+        {
+            position = pos;
+            upQueue = new Queue<Person>();
+            upQueue.Clear();
+            downQueue = new Queue<Person>();
+            downQueue.Clear();
+        }
     }
 }
