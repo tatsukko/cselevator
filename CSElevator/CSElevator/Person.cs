d708bb9f845e9055ba3ce3ceb9649c224ec00042
jsgydjq <jsgydjq@gmail.com>
2012-11-20 15:26:01 +0000

    Initial Version of Class Person. Basic elements and operations included

--- CSElevator/CSElevator/Person.cs
+++ CSElevator/CSElevator/Person.cs
@@ -7,5 +7,28 @@ namespace CSElevator
 {
     class Person
     {
+        public static Queue<Person> ALL;
+        public static int averageWaitingTime;
+        public static int maximumWaitingTime;
+        public static int minimumWaitingTime;
+        public static int averageDuration;
+        public static int maximumDuration;
+        public static int minimumDuration;
+
+        public double startTime;
+        public double endTime;
+        public int targetFloor;
+
+        public void requestElevator(ref Floor floor)
+        {
+            if (targetFloor > floor.position)
+            {
+                floor.upQueue.Enqueue(this);
+            }
+            else
+            {
+                floor.downQueue.Enqueue(this);
+            }
+        }
     }
 }
