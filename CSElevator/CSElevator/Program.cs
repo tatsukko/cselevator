ac817a23191e8c80e2d12b31fc6428ad264a21f8
jsgydjq <jsgydjq@gmail.com>
2012-11-20 15:42:26 +0000

    Program.cs: during simulation, add up time by seconds.
    Elevator.cs: getWork(ref Floor[] floors) Added, to be detailed

--- CSElevator/CSElevator/Program.cs
+++ CSElevator/CSElevator/Program.cs
@@ -47,6 +47,72 @@ namespace CSElevator
         }
         private static void emulate(int TOTAL)
         {
+            double start;
+            Random randTarget = new Random(numOfFloors);
+            Queue<Person>[] ALL = new Queue<Person>[numOfFloors];
+            // 初始化各楼层的乘客信息
+            for (int i = 0; i < numOfFloors; i++)
+            {
+                floors[i] = new Floor(i);
+                ALL[i] = new Queue<Person>();
+                ALL[i].Clear();
+                start = 0;
+                for (int j = 0; j < TOTAL; j++)
+                {
+                    start += Possion.exprand(lamda);
+                    Person p = new Person();
+                    p.startTime = start;
+                    ALL[i].Enqueue(p);
+                }
+            }
+
+            // 每秒模拟
+            for (time = 0; ; time++)
+            {
+                // 检查ALL矩阵, 乘客请求电梯
+                for (int i = 0; i < numOfFloors; i++)
+                {
+                    Person p;
+                    if (ALL[i].Count > 0) p = ALL[i].Peek();
+                    else p = null;
+                    while (p != null && p.startTime < time)
+                    {
+                        ALL[i].Dequeue();
+                        int target = 0;
+                        // 随机目标楼层
+                        while (target == i || target == numOfFloors) target = randTarget.Next(numOfFloors);
+                        p.targetFloor = target;
+                        p.requestElevator(ref floors[i]);
+                        if (ALL[i].Count > 0) p = ALL[i].Peek();
+                        else p = null;
+                    }
+                }
+
+                // 更新楼层参数
+                // 不用了，Floor自己完成
+
+                // 触发电梯
+                int temp = 0;
+                for (int i = 0; i < numOfElevator; i++)
+                {
+                    elevators[i].getWork(ref floors);
+                }
+                if (bOver(ref ALL)) break;
+            }
+        }
+        // Judge whether the whole simulation should be stopped
+        public static bool bOver(ref Queue<Person>[] ALL)
+        {
+            for (int i = 0; i < numOfElevator; i++)
+            {
+                if (elevators[i].direction != 0) return false;
+            }
+            for (int i = 0; i < numOfFloors; i++)
+            {
+                if (floors[i].downQueue.Count > 0 || floors[i].upQueue.Count > 0 || ALL[i].Count > 0)
+                    return false;
+            }
+            return true;
         }
         private static void testPossion()
         {
