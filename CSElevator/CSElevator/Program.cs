b570e8307368893fc43ea3948ab915c2ed08d7a8
jsgydjq <jsgydjq@gmail.com>
2012-11-20 14:56:22 +0000

    Possion generator test OK

--- CSElevator/CSElevator/Program.cs
+++ CSElevator/CSElevator/Program.cs
@@ -9,6 +9,18 @@ namespace CSElevator
     {
         static void Main(string[] args)
         {
+            testPossion();
+        }
+        private static void testPossion()
+        {
+            double total = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                total += Possion.exprand(6);
+            }
+            // Expected output: 10
+            Console.WriteLine(total / 10000);
+            Console.ReadLine();
         }
     }
 }
